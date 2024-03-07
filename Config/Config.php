6d7e90b8e4b8396cc6b72f4d6f5ee10d09ec3665
MattMA21 <matteoarias9568@gmail.com>
2024-03-06 22:04:07 -0500

    forever como wuthag

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+    const BASE_URL = "http://localhost/proyectobiblioteca/";
+    const HOST = "localhost";
+    const BD = "proyectobiblioteca";
+    const DB_USER = "root";
+    const PASS = "";
+    const CHARSET = "charset=utf8";
+?>
